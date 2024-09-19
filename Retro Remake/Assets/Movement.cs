a2b3afbb168304bdbad32ea6a1195663ba7fc5bb
MUKILTEO\1721674 <1721674@mukilteo.wednet.edu>
2024-09-19 09:02:12 -0700

    Made a version of the Pac Man maze and added a player with movements.

---
+++ Retro Remake/Assets/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 12;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetKey() is for Holding key
+        // Input.GetKeyDown() is for Pressing key
+        // Input.GetKeyUp() is for Releasing key
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.right * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.up * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.down * speed;
+        }
+    }
+}
